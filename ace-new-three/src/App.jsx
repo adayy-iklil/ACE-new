9dbfa0298e92d44f985aca2c5297db10abf593e6
adayy-iklil <badariklil6@gmail.com>
2026-07-13 17:39:00 +0700

    feat: initialize App routing configuration and SPA redirect rules for all project versions

--- ace-new-three/src/App.jsx
+++ ace-new-three/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
